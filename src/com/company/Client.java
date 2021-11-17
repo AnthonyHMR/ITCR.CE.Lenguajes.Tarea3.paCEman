c885ff5d6efd1e141c78db04ea00c8a0b83c9986
juanprrr <54974335+juanprrr@users.noreply.github.com>
2021-11-16 18:37:22 -0600

    [Nuevo] Cliente nuevo solo lee info desde consola

--- src/com/company/Client.java
+++ src/com/company/Client.java
@@ -1,72 +1,48 @@
-// A Java program for a Client
+package com.company;// A Java program for a Client
 //source: https://www.geeksforgeeks.org/socket-programming-in-java/
 import java.net.*;
 import java.io.*;
-
+import java.util.Scanner;
 public class Client
 {
-    // initialize socket and input output streams
-    private Socket socket		 = null;
-    private DataInputStream input = null;
-    private DataOutputStream out	 = null;
+    private final Socket socket;
+    private DataInputStream inputt;
+    private Scanner scanner;
+    private byte[] buffer = new byte[512];
+    public Client(String serverAddress, int serverPort) throws Exception {
+        this.socket = new Socket(serverAddress, serverPort);
+        this.scanner = new Scanner(System.in);
+    }
+    protected void start() throws IOException {
+        String input;
+        while (true) {
+            input = scanner.nextLine();
+            PrintWriter out = new PrintWriter(this.socket.getOutputStream(), true);
+            sendMessage(input, out);
+            System.out.println("Recibiendo");
+            System.out.println(reader());
+            buffer = new byte[512];
 
-    // constructor to put ip address and port
-    public Client(String address, int port)
-    {
-        // establish a connection
-        try
-        {
-            socket = new Socket(address, port);
-            System.out.println("Connected");
-
-            // takes input from terminal
-            input = new DataInputStream(System.in);
-
-            // sends output to the socket
-            out = new DataOutputStream(socket.getOutputStream());
-        }
-        catch(UnknownHostException u)
-        {
-            System.out.println(u);
-        }
-        catch(IOException i)
-        {
-            System.out.println(i);
-        }
-
-        // string to read message from input
-        String line = "";
-
-        // keep reading until "Over" is input
-        while (!line.equals("Over"))
-        {
-            try
-            {
-                line = input.readUTF();
-                out.writeUTF(line);
-            }
-            catch(IOException i)
-            {
-                System.out.println(i);
-            }
-        }
-
-        // close the connection
-        try
-        {
-            input.close();
-            out.close();
-            socket.close();
-        }
-        catch(IOException i)
-        {
-            System.out.println(i);
         }
     }
 
-    //public static void main(String args[])
-    //{
-      //  Client client = new Client("127.0.0.1", 8888);
-    //}
+    private void sendMessage(String input, PrintWriter out) throws IOException {
+        inputt = new DataInputStream(this.socket.getInputStream());
+        System.out.println("Enviando: ");
+        System.out.println(input);
+        out.println(input);
+        out.flush();
+    }
+
+    private String reader() throws IOException {
+        String response = "";
+        inputt.read(buffer);
+        for(byte b:buffer){
+            char c = (char)b;
+            response += c;
+        }
+        return response;
+    }
+
 }
 
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,12 @@
 package com.company;
 
-public class Main {
-
-    public static void main(String[] args) {
-	// write your code here
+public class Main
+{
+    public static void main(String[] args) throws Exception {
+        Client client = new Client(
+                "127.0.0.1",
+                8888);
+        client.start();
     }
 }
+
