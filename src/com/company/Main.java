b03499b43a75d2066dd63cadc45401e8a2494850
Anthony Montero <anthonymoro08@gmail.com>
2021-11-18 09:56:45 -0600

    [add] Dots

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,4 +1,10 @@
 package com.company;
+import org.json.simple.parser.*;
+import org.json.simple.*;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 
 public class Main
 {
@@ -8,6 +14,26 @@ public class Main
                 8888);
         client.start();
 
+        /*
+        JSONParser parser = new JSONParser();
+        try {
+            Object obj = parser.parse(new FileReader("src/resources/ejemplo.json"));
+            JSONObject jsonObject = (JSONObject) obj;
+            System.out.println("JSON LEIDO: " + jsonObject);
+            JSONArray jsonArray = (JSONArray) jsonObject.get("partida");
+            System.out.println("");
+            for(int i=0; i<jsonArray.size(); i++){
+                JSONObject jsonObject1 = (JSONObject) jsonArray.get(i);
+
+                System.out.println("DATOS DEL USUARIO: " + i);
+                System.out.println("ghosts: " + jsonObject1.get("ghosts"));
+                System.out.println("fruits: " + jsonObject1.get("fruits"));
+
+                System.out.println("");
+            }
+        }catch (FileNotFoundException e) { }
+        catch (IOException e) { }
+        */
     }
 }
 
